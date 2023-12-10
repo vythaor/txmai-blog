7a909e8884137b3742a30818d34ddf9a29d08088
Sherry <vythao219@gmail.com>
2023-12-10 19:28:51 +0700

    change hash router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 
 const routes = [
@@ -77,7 +77,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(process.env.BASE_URL),
+    history: createWebHistory(process.env.BASE_URL),
     routes,
     scrollBehavior() {
         document.getElementById('app').scrollIntoView();
